6c1f9625e98ed9b1c3f2ffc4a72ebf720a286f1f
Devon Bull <dbull@live.com>
2017-07-18 00:47:04 -0600

    js is out of control, need better way

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 $('.accordion-buttons').on('click', (e) => {
-  let tab = e.target.innerText
-  let test = $('.accordion').children()
-  for (let i = 0; i < test.length; i++) {
-    if (test[i].innerText === tab) {
-      $(test[i]).find('div').removeClass('inactive')
-      $(test[i]).find('div').addClass('active')
-    } else {
-      $(test[i]).find('div').removeClass('active')
-      $(test[i]).find('div').addClass('inactive')
-    }
+  let test = e.target.innerText
+  console.log(test)
+  if (test === 'Tab 1') {
+    
+  } else if (test === 'Tab 2') {
+    let shaka = document.querySelector("[data-id='Tab-2']")
+    console.log(shaka)
   }
 })
