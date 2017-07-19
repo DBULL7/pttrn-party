cb4005a0728d902dd7793fdb77102ce8aed4925b
Devon Bull <dbull@live.com>
2017-07-19 14:12:01 -0600

    css starting to come together, now showing which tab is selected

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ $( document ).ready(() => {
 })
 
 $('.accordion-buttons').on('click', (e) => {
+  let buttons = $(e.target).siblings()
+  for (let i = 0; i < buttons.length; i++) $(buttons[i]).css('background-color', '')
+  $(e.target).css("background-color", "#FFF")
   let clickedTab = e.target.innerText
   let divs = document.querySelectorAll("[data-id]")
   divs.forEach(div => div.dataset.id === clickedTab ? $(div).show() : $(div).hide())
